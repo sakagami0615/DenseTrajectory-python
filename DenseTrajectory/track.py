4cfe3a4de7c3c7ef5b9db67c2a97518a788b1324
tomonori sakagami <sakagami0615@gmail.com>
2020-08-31 22:59:10 +0900

    add:特徴点追跡データ格納クラスの枠組みを作成

---
+++ DenseTrajectory/track.py
@@ -0,0 +1,43 @@
+import numpy
+
+
+class Track:
+	def __init__(self, init_point, track_length, hog_dim, hof_dim, mbhx_dim, mbhy_dim):
+		self.track_num = 0
+		self.hog_descs = numpy.empty((track_length, hog_dim))
+		self.hof_descs = numpy.empty((track_length, hof_dim))
+		self.mbhx_descs = numpy.empty((track_length, mbhx_dim))
+		self.mbhy_descs = numpy.empty((track_length, mbhy_dim))
+
+		self.points = numpy.empty((track_length + 1, 2))
+		self.points[self.track_num,:] = init_point
+	
+	def AddPoint(self, point):
+		self.points[self.track_num,:] = point
+		self.track_num += 1
+	
+	def ResistDescriptor(self, hog_desc, hof_desc, mbhx_desc, mbhy_desc):
+		self.hog_descs[self.track_num,:] = hog_desc
+		self.hof_descs[self.track_num,:] = hof_desc
+		self.mbhx_descs[self.track_num,:] = mbhx_desc
+		self.mbhy_descs[self.track_num,:] = mbhy_desc
+		
+
+
+class TrackList:
+	def __init__(self, track_length, hog_dim, hof_dim, mbhx_dim, mbhy_dim):
+		self.tracks = []
+		self.track_length = track_length
+		self.hog_dim = hog_dim
+		self.hof_dim = hof_dim
+		self.mbhx_dim = mbhx_dim
+		self.mbhy_dim = mbhy_dim
+	
+	def ResistTrack(self, point):
+		track = Track(point, self.track_length, self.hog_dim, self.hof_dim, self.mbhx_dim, self.mbhy_dim)
+		self.tracks.append(track)
+	
+	def RemoveTrack(self, enable_flg):
+		self.tracks = numpy.array(self.tracks)
+		self.tracks = self.tracks[enable_flg]
+		self.tracks = self.tracks.tolist()
